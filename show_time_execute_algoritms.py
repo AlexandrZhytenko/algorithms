37534a6cc4ef736af8bb6e5068330de53b1a6039
Olexandr Zhytenko <zhytenko@ukr.net>
2018-04-18 14:23:36 +0300

    show_time_execute_algoritms

--- show_time_execute_algoritms.py
+++ show_time_execute_algoritms.py
@@ -14,4 +14,9 @@ def show_time_execute_algoritms(arr_len):
         print "For O(n!) where n={} time:".format(i), math.factorial(i) / 10.0, "sec"
 
 # arr_len = [16]
-print show_time_execute_algoritms(arr_len)
+# print show_time_execute_algoritms(arr_len)
+# For O(log n) where n=16 time: 0.4 sec
+# For O(n) where n=16 time: 1.6 sec
+# For O(n log n) where n=16 time: 6.4 sec
+# For O(n**2) where n=16 time: 25.6 sec
+# For O(n!) where n=16 time: 2.0922789888e+12 sec
