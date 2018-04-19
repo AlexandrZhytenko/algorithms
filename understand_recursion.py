74c8a89c33591a7a622582b89c5c85160b962f51
Olexandr Zhytenko <zhytenko@ukr.net>
2018-04-19 08:21:13 +0300

    understand_recursion

--- understand_recursion.py
+++ understand_recursion.py
@@ -6,3 +6,12 @@ def understand_recursion(number):
     understand_recursion(number - 1)
 
 print understand_recursion(3)
+
+# stack recursion (factorial)
+def fact(number):
+    print number
+    if number == 1:
+        return 1
+    return number * fact(number - 1)
+
+print fact(4)
