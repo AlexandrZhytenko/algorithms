11afc355cc849b02d2e7b3d975e243853dd9515c
Olexandr Zhytenko <zhytenko@ukr.net>
2018-04-20 13:18:39 +0300

    Dijkstras algorithm

--- dijkstras_algorithm.py
+++ dijkstras_algorithm.py
@@ -15,8 +15,9 @@ graph["finish"] = {}
 infinity = float("inf")
 costs = {}
 costs["a"] = 6
-costs["b"] = 6
+costs["b"] = 2
 costs["finish"] = infinity
+print "The weight of each node is: ", costs
 
 parents = {}
 parents["a"] = "start"
@@ -24,6 +25,40 @@ parents["b"] = "start"
 parents["finish"] = None
 
 processed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+print "Start: the lowest cost node is", node, "with weight",\
+    graph["start"]["{}".format(node)]
+
+while node is not None:
+    cost = costs[node]
+    print "Continue execution ..."
+    print "The weight of node {} is".format(node), cost
+    neighbors = graph[node]
+    if neighbors != {}:
+        print "The node {} has neighbors:".format(node), neighbors
+    else:
+        print "It is finish, we have the answer: {}".format(cost)
+    for neighbor in neighbors.keys():
+        new_cost = cost + neighbors[neighbor]
+        if costs[neighbor] > new_cost:
+            costs[neighbor] = new_cost
+            parents[neighbor] = node
+    processed.append(node)
+    print "This nodes we researched:", processed
+    node = find_lowest_cost_node(costs)
+    if node is not None:
+        print "Look at the neighbor:", node
 
 # to draw graph
 import networkx
@@ -38,3 +73,6 @@ G.add_edge("b", "finish", weight=5)
 import matplotlib.pyplot as plt
 networkx.draw(G, with_labels=True)
 plt.show()
+
+print "But the shortest path is:", networkx.shortest_path(G, "start", "finish")
+
